61b8f0d1c61173b0ae64525533e9b9f7499f1a9c
Abdul Hannan <abdul.hannan4084@gmail.com>
2024-06-09 15:16:46 +0500

    first commit

---
+++ script2.js
@@ -0,0 +1,13 @@
+/* scripts.js */
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            navbar.style.backgroundColor = '#222';
+        } else {
+            navbar.style.backgroundColor = '#333';
+        }
+    });
+});
